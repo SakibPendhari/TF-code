7254027f6637f592b53b70e44d55d1f597a1661c
sakib pendhari <135151696+SakibPendhari@users.noreply.github.com>
2025-07-26 22:01:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+/*
 resource "aws_instance" "example" {
   ami           = "ami-0623bc4c9a53fe562"
   instance_type = "t2.micro"
@@ -21,3 +22,4 @@ resource "aws_instance" "example" {
     Name = "ExampleInstance"
   }
 }
+*/
