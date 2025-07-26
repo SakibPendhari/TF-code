ba0fc4ea0e12618d5eb4cf7a1cc35341be2c11ab
sakib pendhari <135151696+SakibPendhari@users.noreply.github.com>
2025-07-26 21:11:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1" # Change as needed
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI (for us-east-1)
+  ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
 
   tags = {
